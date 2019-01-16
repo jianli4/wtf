f6b1b0df663ae513a7f0f65151df5b2cf198a758
jianli4 <dirichlet02@gmail.com>
2019-01-16 10:40:51 -0600

    this is a new commit

--- R/session1.R
+++ R/session1.R
@@ -9,3 +9,24 @@ library(usethis)
 library(devtools)
 s<- summary
 h<- utils::head
+
+
+
+devtools::install_version("devtools","1.13.3") ###if you know the version
+
+###if you don't know
+###google the old veresion of that package you are checking
+
+
+library(tidyverse)
+pkgs<- installed.packages("/Library/Frameworks/R.framework/Versions/3.5/Resources/library") %>% as_tibble()
+
+fs::dir_create(Sys.getenv("R_LIBS_USER"))
+.Library
+.libPaths()
+
+library(tidyverse)
+pkgs <- installed.packages("~/Library/R/3.5/library") %>%
+  tibble::as_tibble()
+
+usethis::use_github
